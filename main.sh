d69b32f9b759dc363d1d43b067baba15850ced24
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2021-11-02 22:05:33 +0900

    :art: コメントの書き方を統一

--- main.sh
+++ main.sh
@@ -6,10 +6,10 @@ mkdir ~/repos
 mkdir ~/repos/git
 mkdir ~/repos/unity
 
-#Vim設定ファイルの配置
+# Vim設定ファイルの配置
 cp .vimrc ~/.vimrc
 
-#vim-plugのインストール
+# vim-plugのインストール
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Homebrew及びそのパッケージのインストール
