9bf9cf8b26bd5be46780ca05ebe4f2f2bf814846
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2021-11-02 12:11:34 +0900

    :sparkles: Homebrew及びそのパッケージのインストール処理追加

--- main.sh
+++ main.sh
@@ -9,6 +9,11 @@ mkdir ~/repos/unity
 #Vim設定ファイルの配置
 cp .vimrc ~/.vimrc
 
-
-#vim-plug のインストール
+#vim-plugのインストール
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Homebrew及びそのパッケージのインストール
+cp Brewfile ~/Brewfile
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew bundle --global
