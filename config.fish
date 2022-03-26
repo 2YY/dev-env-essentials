c88b347f41de41ecf9f9f244fe5b8fb96827cd72
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2022-03-26 11:25:31 +0900

    :sparkles: 起動時に tmux を自動で開く処理を追加

--- config.fish
+++ config.fish
@@ -1,3 +1,8 @@
+if status is-interactive
+and not set -q TMUX
+  exec tmux
+end
+
 bind \t accept-autosuggestion
 test -s "$HOME/.kiex/scripts/kiex.fish"; and source "$HOME/.kiex/scripts/kiex.fish"
 starship init fish | source
