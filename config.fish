f749442a1aa61de97368ad9defe073af13b568bf
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2022-05-22 13:39:30 +0900

    :fire: Tmuxをデフォルトで開く処理の削除
    Tmux の利用を終了するため。

--- config.fish
+++ config.fish
@@ -1,8 +1,8 @@
-if status is-interactive
-and not set -q TMUX
-  exec tmux
-end
-
+# タブキーで自動補完を採用する。
 bind \t accept-autosuggestion
+
+# Kiex を使えるようにする。
 test -s "$HOME/.kiex/scripts/kiex.fish"; and source "$HOME/.kiex/scripts/kiex.fish"
+
+# Starship を起動する。
 starship init fish | source
